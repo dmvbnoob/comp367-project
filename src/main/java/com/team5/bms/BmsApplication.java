20ff6a4632bebefc9a39569d2ee7b3414ecc2fbc
LeandroMananquil <andromananquil@yahoo.com>
2024-10-25 16:32:38 -0400

    Added Validation and Logging
    Added Validation and Logging.

--- src/main/java/com/team5/bms/BmsApplication.java
+++ src/main/java/com/team5/bms/BmsApplication.java
@@ -1,13 +1,19 @@
 package com.team5.bms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BmsApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BmsApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("Hello BMS!");
 		SpringApplication.run(BmsApplication.class, args);
+		
 	}
 
 }
