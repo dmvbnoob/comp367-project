285c03142ccb98ad4ed2443157c3441fb63416ab
LeandroMananquil <andromananquil@yahoo.com>
2024-10-25 14:04:21 -0400

    Initial Setup
    Initial Setup by Leandro Mananquil

---
+++ src/main/java/com/team5/bms/BmsApplication.java
@@ -0,0 +1,13 @@
+package com.team5.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team5/bms/BmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team5.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
