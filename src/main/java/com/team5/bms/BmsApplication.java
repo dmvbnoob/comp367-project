cc6b902957ea926d141b35ef8398bc9bf19c15df
Donaldaaaaa <156346855+Donaldaaaaa@users.noreply.github.com>
2024-11-07 13:08:30 -0500

    test

--- src/main/java/com/team5/bms/BmsApplication.java
+++ src/main/java/com/team5/bms/BmsApplication.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//test delete this comment
+
 @SpringBootApplication
 public class BmsApplication {
 	
--- src/main/java/com/team5/bms/model/enumeration/Statuses.java
+++ src/main/java/com/team5/bms/model/enumeration/Statuses.java
@@ -6,11 +6,11 @@ package com.team5.bms.model.enumeration;
  * Service Requests.
  *
  * @author Donalda "Software Quality Manager, Associate Project Manager" Zhuga
- * test only to check the commit is working. delete this one please
- * 
  * 
  * 
  */
 public enum Statuses {
 	CREATED, IN_PROGRESS, COMPLETED,
+	
+	//test
 }
