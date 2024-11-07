6ab5a41b946a81cfe05ea50f644e75740788be7a
Stephanie Santos <stephanie.dayot@gmail.com>
2024-11-07 12:46:49 -0500

    Roles.java (enumeration) has been updated. added: public enum Roles {     TENANT,     OWNER,     ADMINISTRATOR,     SUPERINTENDENT, }

--- src/main/java/com/team5/bms/model/enumeration/Roles.java
+++ src/main/java/com/team5/bms/model/enumeration/Roles.java
@@ -1,5 +1,13 @@
 package com.team5.bms.model.enumeration;
 
+/**
+* This class represents the domain object model for enumeration of User Roles types.
+*
+* @author Stephanie "Business Analysts, Scrum Master, Software Quality" Santos
+*/
 public enum Roles {
-//test
+    TENANT,
+    OWNER,
+    ADMINISTRATOR,
+    SUPERINTENDENT,
 }
