7b604c387f7234b8f4746ebc8c9f3cac2294aa78
Donaldaaaaa <156346855+Donaldaaaaa@users.noreply.github.com>
2024-11-07 13:04:05 -0500

    Statuses.java update

---
+++ src/main/java/com/team5/bms/model/enumeration/Statuses.java
@@ -0,0 +1,16 @@
+
+package com.team5.bms.model.enumeration;
+
+/**
+ * This class represents the domain object model for enumeration of Statuses of
+ * Service Requests.
+ *
+ * @author Donalda "Software Quality Manager, Associate Project Manager" Zhuga
+ * 
+ * 
+ * 
+ * 
+ */
+public enum Statuses {
+	CREATED, IN_PROGRESS, COMPLETED,
+}
