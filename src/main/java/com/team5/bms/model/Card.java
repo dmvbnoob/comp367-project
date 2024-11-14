b32f2efa1f5495fe1514a74118cee78e5925be52
jbelenzo <156251627+jbelenzo@users.noreply.github.com>
2024-11-14 06:37:39 +0000

    Update Card Model and created CardService Service Interface

--- src/main/java/com/team5/bms/model/Card.java
+++ src/main/java/com/team5/bms/model/Card.java
@@ -1,5 +1,153 @@
 package com.team5.bms.model;
 
-public class Card {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import java.io.Serializable;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
+/**
+* This class represents the domain object model for the Bank Card of a Building Owner
+*
+* @author Jasper "Principal Engineer and Software Architect" Belenzo
+*/
+@Entity
+@Table(name = "card")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@SuppressWarnings("common-java:DuplicatedBlocks")
+public class Card implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "number", nullable = false)
+    private String number;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "expiry", nullable = false)
+    private String expiry;
+
+    @NotNull
+    @Column(name = "cvv", nullable = false)
+    private String cvv;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties(value = { "requests", "cards", "building" }, allowSetters = true)
+    private Uuser user;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public Card id(Long id) {
+        this.setId(id);
+        return this;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
+
+    public Card number(String number) {
+        this.setNumber(number);
+        return this;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Card name(String name) {
+        this.setName(name);
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExpiry() {
+        return this.expiry;
+    }
+
+    public Card expiry(String expiry) {
+        this.setExpiry(expiry);
+        return this;
+    }
+
+    public void setExpiry(String expiry) {
+        this.expiry = expiry;
+    }
+
+    public String getCvv() {
+        return this.cvv;
+    }
+
+    public Card cvv(String cvv) {
+        this.setCvv(cvv);
+        return this;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public Buser getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Card user(User user) {
+        this.setUser(user);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Card)) {
+            return false;
+        }
+        return getId() != null && getId().equals(((Card) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Card{" +
+            "id=" + getId() +
+            ", number='" + getNumber() + "'" +
+            ", name='" + getName() + "'" +
+            ", expiry='" + getExpiry() + "'" +
+            ", cvv='" + getCvv() + "'" +
+            "}";
+    }
 }
