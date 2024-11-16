5689099d29897fa0a6135749c0627b0e88e76db3
LeandroMananquil <andromananquil@yahoo.com>
2024-11-16 17:52:05 -0500

    Created UserResource.java rest Controller
    Created UserResource.java rest Controller

---
+++ src/main/java/com/team5/bms/service/impl/UserServiceImpl.java
@@ -0,0 +1,96 @@
+package com.team5.bms.service.impl;
+ 
+import com.team5.bms.model.User;
+import com.team5.bms.repository.UserRepository;
+import com.team5.bms.service.UserService;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+ 
+/**
+* Service Implementation for managing {@link com.team5.bms.model.User}.
+*
+* @author Leandro "The Project Manager" Mananquil
+*/
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+ 
+    private static final Logger LOG = LoggerFactory.getLogger(UserServiceImpl.class);
+ 
+    private final UserRepository userRepository;
+ 
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+ 
+    @Override
+    public User save(User user) {
+        LOG.debug("Request to save User : {}", user);
+        return userRepository.save(user);
+    }
+ 
+    @Override
+    public User update(User user) {
+        LOG.debug("Request to update User : {}", user);
+        return userRepository.save(user);
+    }
+ 
+    @Override
+    public Optional<User> partialUpdate(User user) {
+        LOG.debug("Request to partially update User : {}", user);
+ 
+        return userRepository
+            .findById(user.getId())
+            .map(existingUser -> {
+                if (user.getUsername() != null) {
+                    existingUser.setUsername(user.getUsername());
+                }
+                if (user.getPassword() != null) {
+                    existingUser.setPassword(user.getPassword());
+                }
+                if (user.getFirstname() != null) {
+                    existingUser.setFirstname(user.getFirstname());
+                }
+                if (user.getLastname() != null) {
+                    existingUser.setLastname(user.getLastname());
+                }
+                if (user.getEmail() != null) {
+                    existingUser.setEmail(user.getEmail());
+                }
+                if (user.getPhone() != null) {
+                    existingUser.setPhone(user.getPhone());
+                }
+                if (user.getRole() != null) {
+                    existingUser.setRole(user.getRole());
+                }
+ 
+                return existingUser;
+            })
+            .map(userRepository::save);
+    }
+ 
+    @Override
+    @Transactional(readOnly = true)
+    public Page<User> findAll(Pageable pageable) {
+        LOG.debug("Request to get all Users");
+        return userRepository.findAll(pageable);
+    }
+ 
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<User> findOne(Long id) {
+        LOG.debug("Request to get User : {}", id);
+        return userRepository.findById(id);
+    }
+ 
+    @Override
+    public void delete(Long id) {
+        LOG.debug("Request to delete User : {}", id);
+        userRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/team5/bms/web/rest/UserResource.java
@@ -0,0 +1,176 @@
+package com.team5.bms.web.rest;
+ 
+import com.team5.bms.model.User;
+import com.team5.bms.repository.UserRepository;
+import com.team5.bms.service.UserService;
+import com.team5.bms.web.rest.errors.BadRequestAlertException;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+ 
+ 
+/**
+* REST controller for managing {@link ca.test.bms.domain.User}.
+* 
+* @author Leandro "The Project Manager" Mananquil
+*/
+@RestController
+@RequestMapping("/api/users")
+public class UserResource {
+ 
+    private static final Logger LOG = LoggerFactory.getLogger(UserResource.class);
+ 
+    private static final String ENTITY_NAME = "user";
+ 
+    private String applicationName = "bmsApp";
+ 
+    private final UserService userService;
+ 
+    private final UserRepository userRepository;
+ 
+    public UserResource(UserService userService, UserRepository userRepository) {
+        this.userService = userService;
+        this.userRepository = userRepository;
+    }
+ 
+    /**
+     * {@code POST  /users} : Create a new user.
+     *
+     * @param user the user to create.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new user, or with status {@code 400 (Bad Request)} if the user has already an ID.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PostMapping("")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) throws URISyntaxException {
+        LOG.debug("REST request to save User : {}", user);
+        if (user.getId() != null) {
+            throw new BadRequestAlertException("A new user cannot already have an ID", ENTITY_NAME, "idexists");
+        }
+        user = userService.save(user);
+        return ResponseEntity.created(new URI("/api/users/" + user.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, user.getId().toString()))
+            .body(user);
+    }
+ 
+    /**
+     * {@code PUT  /users/:id} : Updates an existing user.
+     *
+     * @param id the id of the user to save.
+     * @param user the user to update.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated user,
+     * or with status {@code 400 (Bad Request)} if the user is not valid,
+     * or with status {@code 500 (Internal Server Error)} if the user couldn't be updated.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable(value = "id", required = false) final Long id, @Valid @RequestBody User user)
+        throws URISyntaxException {
+        LOG.debug("REST request to update User : {}, {}", id, user);
+        if (user.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        if (!Objects.equals(id, user.getId())) {
+            throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
+        }
+ 
+        if (!userRepository.existsById(id)) {
+            throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
+        }
+ 
+        user = userService.update(user);
+        return ResponseEntity.ok()
+            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, user.getId().toString()))
+            .body(user);
+    }
+ 
+    /**
+     * {@code PATCH  /users/:id} : Partial updates given fields of an existing user, field will ignore if it is null
+     *
+     * @param id the id of the user to save.
+     * @param user the user to update.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated user,
+     * or with status {@code 400 (Bad Request)} if the user is not valid,
+     * or with status {@code 404 (Not Found)} if the user is not found,
+     * or with status {@code 500 (Internal Server Error)} if the user couldn't be updated.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PatchMapping(value = "/{id}", consumes = { "application/json", "application/merge-patch+json" })
+    public ResponseEntity<User> partialUpdateUser(
+        @PathVariable(value = "id", required = false) final Long id,
+        @NotNull @RequestBody User user
+    ) throws URISyntaxException {
+        LOG.debug("REST request to partial update User partially : {}, {}", id, user);
+        if (user.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        if (!Objects.equals(id, user.getId())) {
+            throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
+        }
+ 
+        if (!userRepository.existsById(id)) {
+            throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
+        }
+ 
+        Optional<User> result = userService.partialUpdate(user);
+ 
+        return ResponseUtil.wrapOrNotFound(
+            result,
+            HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, user.getId().toString())
+        );
+    }
+ 
+    /**
+     * {@code GET  /users} : get all the users.
+     *
+     * @param pageable the pagination information.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of users in body.
+     */
+    @GetMapping("")
+    public ResponseEntity<List<User>> getAllUsers(@org.springdoc.core.annotations.ParameterObject Pageable pageable) {
+        LOG.debug("REST request to get a page of Users");
+        Page<User> page = userService.findAll(pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
+    }
+ 
+    /**
+     * {@code GET  /users/:id} : get the "id" user.
+     *
+     * @param id the id of the user to retrieve.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the user, or with status {@code 404 (Not Found)}.
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable("id") Long id) {
+        LOG.debug("REST request to get User : {}", id);
+        Optional<User> user = userService.findOne(id);
+        return ResponseUtil.wrapOrNotFound(user);
+    }
+ 
+    /**
+     * {@code DELETE  /users/:id} : delete the "id" user.
+     *
+     * @param id the id of the user to delete.
+     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("id") Long id) {
+        LOG.debug("REST request to delete User : {}", id);
+        userService.delete(id);
+        return ResponseEntity.noContent()
+            .headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString()))
+            .build();
+    }
+}
