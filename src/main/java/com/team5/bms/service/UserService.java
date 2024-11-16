2df502051ea30204337b4bced546bff680f804fc
LeandroMananquil <andromananquil@yahoo.com>
2024-11-16 10:48:13 -0500

    Create UserService.java
    Created java class interface name UserService.java

---
+++ src/main/java/com/team5/bms/service/UserService.java
@@ -0,0 +1,64 @@
+package com.team5.bms.service;
+
+import java.util.Optional;
+//import com.team5.bms.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import com.team5.bms.model.User;
+
+/**
+* Service Interface for managing {@link com.team5.bms.domain.User}.
+*
+* @author Leandro "The Project Manager" Mananquil
+*/
+
+public interface UserService {
+	
+    /**
+     * Save a user.
+     *
+     * @param user the entity to save.
+     * @return the persisted entity.
+     */
+    User save(User user);
+ 
+    /**
+     * Updates a User.
+     *
+     * @param user the entity to update.
+     * @return the persisted entity.
+     */
+    User update(User user);
+ 
+    /**
+     * Partially updates a user.
+     *
+     * @param user the entity to update partially.
+     * @return the persisted entity.
+     */
+    Optional<User> partialUpdate(User user);
+ 
+    /**
+     * Get all the users.
+     *
+     * @param pageable the pagination information.
+     * @return the list of entities.
+     */
+    Page<User> findAll(Pageable pageable);
+ 
+    /**
+     * Get the "id" user.
+     *
+     * @param id the id of the entity.
+     * @return the entity.
+     */
+    Optional<User> findOne(Long id);
+ 
+    /**
+     * Delete the "id" user.
+     *
+     * @param id the id of the entity.
+     */
+    void delete(Long id);	
+
+}
