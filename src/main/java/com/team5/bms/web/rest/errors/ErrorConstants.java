e9c2d59554202f1bece2e3e0e18c67aaf19eb6e9
jbelenzo <156251627+jbelenzo@users.noreply.github.com>
2024-11-17 04:14:47 +0000

    Updated pom.xml and ErrorConstants.java

--- src/main/java/com/team5/bms/web/rest/errors/ErrorConstants.java
+++ src/main/java/com/team5/bms/web/rest/errors/ErrorConstants.java
@@ -6,7 +6,9 @@ public final class ErrorConstants {
 
     public static final String ERR_CONCURRENCY_FAILURE = "error.concurrencyFailure";
     public static final String ERR_VALIDATION = "error.validation";
+    
     public static final String PROBLEM_BASE_URL = "http://localhost:8080";
+    // TODO Create Error Views for the following
     public static final URI DEFAULT_TYPE = URI.create(PROBLEM_BASE_URL + "/problem-with-message");
     public static final URI CONSTRAINT_VIOLATION_TYPE = URI.create(PROBLEM_BASE_URL + "/constraint-violation");
     public static final URI INVALID_PASSWORD_TYPE = URI.create(PROBLEM_BASE_URL + "/invalid-password");
