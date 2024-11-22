7c9011df8223392423e5b9225fb064dd28395fc5
jbelenzo <156251627+jbelenzo@users.noreply.github.com>
2024-11-22 23:08:15 +0000

    Added initial HomeController.java and index.html

---
+++ src/main/java/com/team5/bms/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.team5.bms.web.rest;
+ 
+import com.team5.bms.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/index")
+    public String showHomePage() {
+        return "index";
+    }
+
+}
