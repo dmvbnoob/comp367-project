28ab23d2c0bf2d43126f1a1b670e644742020868
jbelenzo <156251627+jbelenzo@users.noreply.github.com>
2024-11-24 10:04:58 +0000

    Updating Registration for Building Owner User

--- src/main/java/com/team5/bms/controller/UserController.java
+++ src/main/java/com/team5/bms/controller/UserController.java
@@ -29,7 +29,14 @@ public class UserController {
     @Autowired
     private ObjectMapper objectMapper;
 
-    String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+    // String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+
+    private String baseUrl;
+
+    public UserController() {
+        // This will throw an exception if there is no HTTP request context
+        this.baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+    }
 
     @GetMapping("/register")
     public String showRegisterPage(Model model) {
