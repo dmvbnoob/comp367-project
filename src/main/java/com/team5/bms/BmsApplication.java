1cba767d9e64f6bbbaf3528c699e838e1ba43de7
jbelenzo <156251627+jbelenzo@users.noreply.github.com>
2024-11-26 05:38:26 +0000

    Added Default Existing 'Team 5 Tower' Building

--- src/main/java/com/team5/bms/BmsApplication.java
+++ src/main/java/com/team5/bms/BmsApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.team5.bms.repository.*;
 import com.team5.bms.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class BmsApplication {
+public class BmsApplication implements CommandLineRunner {
 	
 	private static final Logger logger = LoggerFactory.getLogger(BmsApplication.class);
 
@@ -30,7 +32,7 @@ public class BmsApplication {
 		team5Tower.setAddress("300 Borough Drive, Scarborough, ON, M1P 4P5");
 		team5Tower.setBuildingPhone("+19876543210");
 		buildingRepository.save(team5Tower);
-		team5Tower = buildingRepository.findById(1);
+		team5Tower = buildingRepository.findById(Long.valueOf(1L));
 		System.out.println("BmsApplication - run - team5Tower - id -> " + team5Tower.getId());
 	
 	}
