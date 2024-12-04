72aa08ac78bd69d7e8fe6f7953e02ce5d9563caf
jbelenzo <156251627+jbelenzo@users.noreply.github.com>
2024-12-04 11:37:37 +0000

    Completing all User related functionalities and Bug Fixing

--- src/main/java/com/team5/bms/repository/UserRepository.java
+++ src/main/java/com/team5/bms/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.team5.bms.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import com.team5.bms.model.User;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
